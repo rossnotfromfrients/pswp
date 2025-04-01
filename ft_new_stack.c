cac92655ca8e0473295dc2f937a11e83dbae0483
Rostyslav Dolhoplov <rodolhop@e3r8p5.42roma.it>
2025-04-01 16:31:59 +0200

    Removed libft from tracking and added to .gitignore

--- ft_new_stack.c
+++ ft_new_stack.c
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_error.c                                      :+:      :+:    :+:   */
+/*   ft_new_stack.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rodolhop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 14:51:07 by rodolhop          #+#    #+#             */
-/*   Updated: 2025/04/01 14:51:08 by rodolhop         ###   ########.fr       */
+/*   Created: 2025/04/01 16:24:57 by rodolhop          #+#    #+#             */
+/*   Updated: 2025/04/01 16:24:59 by rodolhop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include  "push_swap.h"
+#include "push_swap.h"
 
-void ft_error(void)
+t_stack *ft_new_stack(int digit)
 {
-    write(2, "error\n", 6);
-    exit(1);
+    t_stack *new;
+
+    new = malloc(sizeof (t_stack));
+    if (!new)
+        ft_error();
+    new->nbr = digit;
+    new->next = NULL;
+    return (new);
 }
